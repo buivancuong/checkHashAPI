8e57029b7889978533fe514d1aaa9b3c222c25a0
buivancuong <acsimethd@gmail.com>
2020-05-18 10:18:12 +0700

    change ip

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def check_hash():
 #if __name__ == '__main__':
 #    app.run()
 if __name__ == '__main__':
-     app.run(host='127.0.0.1', port=5050, debug=True, ssl_context=('server.crt', 'server.key'))
+     app.run(host='0.0.0.0', port=5050, debug=True, ssl_context=('server.crt', 'server.key'))
